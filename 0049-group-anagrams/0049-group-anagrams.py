6df81d9fc688640657a65c6bae9538cb642dc17d
소희 <138556584+esoby@users.noreply.github.com>
2024-01-03 19:37:39 +0900

    Time: 87 ms (91.76%), Space: 20.8 MB (41.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = {}
+        my_dict = {}
 
         for s in strs:
             sorted_s = ''.join(sorted(s))
-            d[sorted_s] = d[sorted_s] + [s] if d.get(sorted_s) else [s]
 
-        return d.values()
+            if my_dict.get(sorted_s):
+                my_dict[sorted_s] += [s]
+            else:
+                my_dict[sorted_s] = [s]
+
+        return my_dict.values()
