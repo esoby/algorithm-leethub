6e5bce94cb16d14332f4fdaacd948173d7457354
소희 <138556584+esoby@users.noreply.github.com>
2024-01-05 20:51:30 +0900

    Time: 44 ms (24.59%), Space: 17.3 MB (7.67%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,16 +1,21 @@
+from functools import reduce 
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        my_dict = {}
+        # my_dict = {}
         
-        for stone in stones:
-            if stone in my_dict:
-                my_dict[stone] += 1
-            else:
-                my_dict[stone] = 1
+        # for stone in stones:
+        #     if stone in my_dict:
+        #         my_dict[stone] += 1
+        #     else:
+        #         my_dict[stone] = 1
         
-        cnt = 0
-        for jewel in jewels:
-            if jewel in my_dict:
-                cnt += my_dict[jewel]
+        # cnt = 0
+        # for jewel in jewels:
+        #     if jewel in my_dict:
+        #         cnt += my_dict[jewel]
         
-        return cnt
+        # return cnt
+
+        jewels = set(jewels)
+        return reduce(lambda acc, cur: acc + (cur in jewels), stones, 0)
