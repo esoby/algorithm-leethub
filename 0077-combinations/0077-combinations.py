a88628f97b04020d3dd6015c9a5774ffdfa0cd8b
소희 <138556584+esoby@users.noreply.github.com>
2024-01-10 17:55:47 +0900

    Time: 693 ms (96.10%), Space: 65.8 MB (13.65%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n: int, k: int):
+        ans = []
+        stack = [(i, [i]) for i in range(1, n+1)]
+
+        while stack:
+            num, tmp = stack.pop()
+            if len(tmp) == k:
+                ans.append(tmp)
+            elif num < n:
+                for i in range(num+1, n+1):
+                    stack.append((i, tmp + [i]))
+
+        return ans
+
+# from itertools import combinations
+# class Solution:
+#     def combine(self, n: int, k: int) -> List[List[int]]:
+#         x = 0
+#         nums = [x+1 for x in range(n)]
+#         return combinations(nums, k)
