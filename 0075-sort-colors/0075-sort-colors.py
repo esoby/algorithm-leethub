e0089d09a0e147fd0ffff8c94466e6b673c70fad
소희 <138556584+esoby@users.noreply.github.com>
2024-01-17 18:14:53 +0900

    Time: 46 ms (23.89%), Space: 17.5 MB (17.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,3 +1,13 @@
+# 버블 정렬
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+    def sortColors(self, nums):
+        iters = len(nums) - 1
+
+        for iter in range(iters):
+            wall = iters - iter
+            print(wall)
+            for cur in range(wall):
+                if nums[cur] > nums[cur + 1]:
+                    nums[cur], nums[cur + 1] = nums[cur + 1], nums[cur]
+
+        return nums
