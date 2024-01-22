c512324286f9d033100045d0dfb8ab5da7e23f6b
소희 <138556584+esoby@users.noreply.github.com>
2024-01-22 09:35:38 +0900

    Time: 595 ms (44.91%), Space: 30.9 MB (58.85%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,12 +3,11 @@ class Solution:
         if not nums:
             return 0
 
-        memo = [0] * len(nums)
-        memo[0] = nums[0]
         max_val = nums[0]
 
         for i in range(1, len(nums)):
-            memo[i] = max(nums[i], memo[i-1] + nums[i])
-            max_val = max(max_val, memo[i])
+            # i에서 새로 시작하기 vs 그동안의 부분 배열 합에 더하기
+            nums[i] = max(nums[i], nums[i-1] + nums[i])
+            max_val = max(max_val, nums[i])
 
         return max_val
